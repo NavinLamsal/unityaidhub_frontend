83bb0fab71257d24172521f67131068d5f697cc6
Navin <95266285+NavinLamsal@users.noreply.github.com>
2024-01-03 11:08:35 +0545

    fixed build error

--- app/browse-a-campaign/page.tsx
+++ app/browse-a-campaign/page.tsx
@@ -1,11 +1,10 @@
-import CampaingCard from '@/components/Card/CampaingCard'
+
 import FundraisingCard from '@/components/Card/fundraisingCard'
 import React from 'react'
 
 const Page = () => {
   return (
     <div>
-      <CampaingCard/>
       <FundraisingCard/>
     </div>
   )
